ae1cf2c17d5e3acc21432b6e95f1b9e9d4b23bbe
silverfox <abajur55@mail.ru>
2021-05-16 21:10:25 +0200

    change calculateDcg.js

--- src/games/brain-dcg/calculateDcg.js
+++ src/games/brain-dcg/calculateDcg.js
@@ -1,12 +1,15 @@
 const calculateDcg = (a, b) => {
   let newA = a;
   let newB = b;
+  if (newA === 0 || newB === 0) {
+    return 1;
+  }
   if (newA % newB === 0) {
     return newB;
   }
 
   const iter = (counter, acc) => {
-    if (acc === 0) {
+    if (acc === 0 || counter === 0) {
       return 1;
     }
     if (counter % acc === 0) {
